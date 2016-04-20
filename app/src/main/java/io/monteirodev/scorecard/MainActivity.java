ab5adbad31653b8b2a979d1e07950807d8add7c2
Carlos Monteiro <unomonteiro@gmail.com>
2016-04-20 17:49:27 +0100

    Created Hole Object and initialize it in MainActivity

--- app/src/main/java/io/monteirodev/scorecard/MainActivity.java
+++ app/src/main/java/io/monteirodev/scorecard/MainActivity.java
@@ -6,11 +6,18 @@ import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
 
+    private Hole[] mHoles = new Hole[18];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Initialize holes
+        int strokes = 0;
+        for (int i = 0; i < mHoles.length; i++) {
+            mHoles[i] = new Hole("Hole "+ (i + 1) + " :", strokes);
+
+        }
 
     }
 }
