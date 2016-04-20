35e1e150e77c7d5118c403c6a7d79388abe43060
Carlos Monteiro <unomonteiro@gmail.com>
2016-04-20 19:12:37 +0100

    Implemented SharedPreferences

--- app/src/main/java/io/monteirodev/scorecard/MainActivity.java
+++ app/src/main/java/io/monteirodev/scorecard/MainActivity.java
@@ -1,11 +1,16 @@
 package io.monteirodev.scorecard;
 
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends ListActivity {
-
+    private static final String PREFS_FILE = "io.monteirodev.scorecard.preferences";
+    private static final String KEY_STROKECOUNT = "key_strokecount";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
     private Hole[] mHoles = new Hole[18];
     private ListAdapter mListAdapter;
 
@@ -13,15 +18,28 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSharedPreferences = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
 
         // Initialize holes
-        int strokes = 0;
+        int strokes;
         for (int i = 0; i < mHoles.length; i++) {
+            strokes = mSharedPreferences.getInt(KEY_STROKECOUNT + i,0);
             mHoles[i] = new Hole("Hole "+ (i + 1) + " :", strokes);
         }
         // this == getApplicationContext() == MainActivity
         mListAdapter = new ListAdapter(this, mHoles);
         setListAdapter(mListAdapter);
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        for (int i = 0; i < mHoles.length; i++) {
+            mEditor.putInt(KEY_STROKECOUNT + i, mHoles[i].getStrokeCount());
+        }
+        mEditor.apply();
     }
 }
