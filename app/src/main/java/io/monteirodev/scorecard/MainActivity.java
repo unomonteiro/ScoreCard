b8a588f7c81c4a6511f6793f74977b011c0a6850
Carlos Monteiro <unomonteiro@gmail.com>
2016-04-20 17:41:37 +0100

    Created Layout, ListView and list Item

--- app/src/main/java/io/monteirodev/scorecard/MainActivity.java
+++ app/src/main/java/io/monteirodev/scorecard/MainActivity.java
@@ -1,15 +1,16 @@
 package io.monteirodev.scorecard;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+
     }
 }
